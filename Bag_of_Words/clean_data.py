e8862fc92cc47cf4cbdb6c391e70b06e35731124
Mikhail Filipchuk <mfilipch@e1z2r8p7.42.us.org>
2017-04-07 01:29:06 -0700

    validation 96.2% achieved

--- Bag_of_Words/clean_data.py
+++ Bag_of_Words/clean_data.py
@@ -9,16 +9,18 @@ import re
 
 data = pd.read_csv("labeledTrainData.tsv", header=0,\
  delimiter="\t", quoting=3)
+test_data = pd.read_csv("testData.tsv", header=0,\
+ delimiter="\t", quoting=3)
 all_i = np.arange(len(data))
-train_i, test_i = train_test_split(all_i, train_size=0.8,\
- random_state = 44 )
-#print(all_i, train_i, test_i)
+train_i, validation_i = train_test_split(all_i, train_size=0.8,\
+ random_state = 43 )
+#print(all_i, train_i, validation_i)
 train = data.ix[train_i]
-test = data.ix[test_i]
+validation = data.ix[validation_i]
 stops = set(stopwords.words("english"))
 
 num_tr_reviews = train["review"].size
-num_ts_reviews = test["review"].size
+num_ts_reviews = validation["review"].size
 
 def review_to_words(raw_review, stops):
 	# Get HTML tags out
@@ -28,20 +30,25 @@ def review_to_words(raw_review, stops):
 	# Lowercase and split to words
 	words = letters_only.lower().split()
 	# Exclude stopwords
-	words = [w for w in words if not w in stops]
+	words = [w for w in words if (w == "not" or not w in stops)]
 	return " ".join(words)
 
-# Make clean train and test reviews
+# Make clean train and validation reviews
 clean_train_reviews = []
+clean_validation_reviews = []
 clean_test_reviews = []
+for el in test_data["review"]:
+	clean_test_reviews.append(review_to_words(el, stops))
 for i in train_i:
 	clean_train_reviews.append(review_to_words(train["review"][i], stops))
-for i in test_i:
-	clean_test_reviews.append(review_to_words(test["review"][i], stops))
+for i in validation_i:
+	clean_validation_reviews.append(review_to_words(validation["review"][i], stops))
 
 train_data = pd.DataFrame(data={"id":train_i, "sentiment": train["sentiment"], "review":clean_train_reviews})
-test_data = pd.DataFrame(data={"id":test_i, "sentiment": test["sentiment"], "review":clean_test_reviews})
+validation_data = pd.DataFrame(data={"id":validation_i, "sentiment": validation["sentiment"], "review":clean_validation_reviews})
+test_data = pd.DataFrame(data={"id":test_data["id"], "review":clean_test_reviews})
 
 # Save cleaned data
 train_data.to_csv( "Clean_train.csv", index=False, header=0, quoting=3 )
+validation_data.to_csv( "Clean_validation.csv", index=False, header=0, quoting=3 )
 test_data.to_csv( "Clean_test.csv", index=False, header=0, quoting=3 )
--- Bag_of_Words/regression.py
+++ Bag_of_Words/regression.py
@@ -12,36 +12,37 @@ from sklearn.metrics import auc, roc_auc_score
 
 import matplotlib.pyplot as plt
 
-MAX_FEAT = 5000
+MAX_FEAT = 7000
 NUM_CLASSES = 1
 LEARNING_RATE = 2
 TRAIN_STEPS = 100
 BATCH_SIZE = 2000
 TEMP_SIZE = 5000
-NGRAM = 2
+NGRAM = 3
+BETA_INIT = 4e-3
 
 MAX_FEAT *= NGRAM
 
 train_data = pd.read_csv("Clean_train.csv", names=['id', 'reviews', 'sentiment'], quoting=3)
-test_data = pd.read_csv("Clean_test.csv", names=['id', 'reviews', 'sentiment'], quoting=3)
+validation_data = pd.read_csv("Clean_validation.csv", names=['id', 'reviews', 'sentiment'], quoting=3)
 
 TRAIN_SIZE = len(train_data['sentiment'])
-TEST_SIZE = len(test_data['sentiment'])
+validation_SIZE = len(validation_data['sentiment'])
 
-#print(test_data)
-# vectorizer = CountVectorizer(analyzer = "word",  \
-# 							tokenizer = None,    \
-# 							preprocessor = None, \
-# 							stop_words = None,   \
-# 							ngram_range=(1, NGRAM),  \
-# 							max_features = MAX_FEAT)
-
-vectorizer = TfidfVectorizer(analyzer = "word",  \
+#print(validation_data)
+vectorizer = CountVectorizer(analyzer = "word",  \
 							tokenizer = None,    \
 							preprocessor = None, \
 							stop_words = None,   \
 							ngram_range=(1, NGRAM),  \
 							max_features = MAX_FEAT)
+
+# vectorizer = TfidfVectorizer(analyzer = "word",  \
+# 							tokenizer = None,    \
+# 							preprocessor = None, \
+# 							stop_words = None,   \
+# 							ngram_range=(1, NGRAM),  \
+# 							max_features = MAX_FEAT)
 
 #transformer = TfidfTransformer(smooth_idf=False)
 
@@ -52,15 +53,15 @@ x_train = x_train_raw.toarray()
 y_train = np.reshape(train_data['sentiment'].values, (TRAIN_SIZE, NUM_CLASSES))
 # y_train = np.reshape(train_data[['sentiment', 'asentiment']].values, (TRAIN_SIZE, NUM_CLASSES))
 vocab = vectorizer.get_feature_names()
-x_test_raw = vectorizer.transform(test_data['reviews'])
-#x_test_raw = transformer.fit_transform(x_test_raw)
-x_test = x_test_raw.toarray()
-# test_data['asentiment'] = 1 - test_data['sentiment']
-y_test = np.reshape(test_data['sentiment'].values, (TEST_SIZE, NUM_CLASSES))
-# y_test = np.reshape(test_data[['sentiment', 'asentiment']].values, (TEST_SIZE, NUM_CLASSES))
+x_validation_raw = vectorizer.transform(validation_data['reviews'])
+#x_validation_raw = transformer.fit_transform(x_validation_raw)
+x_validation = x_validation_raw.toarray()
+# validation_data['asentiment'] = 1 - validation_data['sentiment']
+y_validation = np.reshape(validation_data['sentiment'].values, (validation_SIZE, NUM_CLASSES))
+# y_validation = np.reshape(validation_data[['sentiment', 'asentiment']].values, (validation_SIZE, NUM_CLASSES))
 
 print(x_train.shape, y_train.shape)
-print(x_test.shape, y_test.shape)
+print(x_validation.shape, y_validation.shape)
 
 sess = None
 def ResetSession():
@@ -72,6 +73,7 @@ def ResetSession():
 ResetSession()
 x = tf.placeholder(tf.float32, [None, MAX_FEAT], name='x')
 y = tf.placeholder(tf.float32, [None, NUM_CLASSES], name='y_label')
+beta = tf.placeholder(tf.float32, name='beta')
 
 def weight_variable(inputs, outputs, name):
 	# Random small values
@@ -106,53 +108,83 @@ b = bias_variable(NUM_CLASSES, name='bias')
 h = tf.matmul(x, W) + b
 h_sig = tf.sigmoid(h)
 # loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=h, labels=y))
-loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=h, labels=y))
+regularizer = tf.nn.l2_loss(W)
+loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=h, labels=y) + beta * regularizer)
 # train_step = tf.train.GradientDescentOptimizer(LEARNING_RATE).minimize(loss)
-train_step = tf.train.AdamOptimizer(4e-3).minimize(loss)
+train_step = tf.train.AdamOptimizer(1e-3).minimize(loss)
 
+h_class = tf.cast((h_sig > 0.5), tf.float32)
 prediction = tf.equal((h_sig > 0.5), tf.cast(y, tf.bool))
 accuracy = tf.reduce_mean(tf.cast(prediction, tf.float32))
 
+
+
 init_g = tf.global_variables_initializer()
-sess.run(init_g)
 
-tr_loss_lst = []
-ts_loss_lst = []
+
+
 #i_lst = []
-i = 0
-avg_loss = 0
-batches = batch_iter(zip(x_train, y_train), BATCH_SIZE, TRAIN_STEPS)
-for batch in batches:
-	x_batch, y_batch = zip(*batch)
-	#i_lst.append(i)
-	i_loss, _ = sess.run([loss, train_step], feed_dict={x: x_batch, y: y_batch})
-	
-	avg_loss += i_loss
-	if i % 10 == 0:
-		print(int((i * 10) / TRAIN_STEPS), i_loss)
-		tr_loss_lst.append(avg_loss/10)
-		avg_loss = 0
-		ts_loss_lst.append(sess.run(accuracy, feed_dict={x: x_test, y: y_test}))
-	if i % 100 == 0:
-		print("Train accuracy %f" % sess.run(accuracy, feed_dict={x: x_train, y: y_train}))
-		# sess.run(init_l)
-		exh, exy = sess.run([h_sig, y], feed_dict={x: x_batch, y: y_batch})
-		print("Train AUC %f" % roc_auc_score(exy[:,0],exh[:,0]))
-	i += 1
-print("Train accuracy %f" % sess.run(accuracy, feed_dict={x: x_train, y: y_train}))
-exh, exy = sess.run([h_sig, y], feed_dict={x: x_train, y: y_train})
-print("Train AUC %f" % roc_auc_score(exy[:,0],exh[:,0]))
-print("Test accuracy %f" % sess.run(accuracy, feed_dict={x: x_test, y: y_test}))
-exh, exy = sess.run([h_sig, y], feed_dict={x: x_test, y: y_test})
-print("Test AUC %f" % roc_auc_score(exy[:,0],exh[:,0]))
+j_beta = BETA_INIT
+mult = 2
+j_tr_loss = []
+j_vl_loss = []
+j_beta_lst = []
+for j in range(4):
+	# tr_loss_lst = []
+	# ts_loss_lst = []
+	sess.run(init_g)
+	i = 0
+	avg_loss = 0
+	batches = batch_iter(zip(x_train, y_train), BATCH_SIZE, TRAIN_STEPS)
+	for batch in batches:
+		x_batch, y_batch = zip(*batch)
+		#i_lst.append(i)
+		i_loss, _ = sess.run([loss, train_step], feed_dict={x: x_batch, y: y_batch, beta: j_beta})
+		
+		# avg_loss += i_loss
+		# if i % 10 == 0:
+			# print(int((i * 10) / TRAIN_STEPS), i_loss)
+			# tr_loss_lst.append(avg_loss/10)
+			# avg_loss = 0
+			# ts_loss_lst.append(sess.run(accuracy, feed_dict={x: x_validation, y: y_validation, beta: 0}))
+		if i % 100 == 0:
+			print("Train accuracy %f" % sess.run(accuracy, feed_dict={x: x_train, y: y_train}))
+			# sess.run(init_l)
+			exh, exy = sess.run([h_sig, y], feed_dict={x: x_batch, y: y_batch})
+			print("Train AUC %f" % roc_auc_score(exy[:,0],exh[:,0]))
+		i += 1
+	print("%d, beta %f" % (j, j_beta))
+	j_beta_lst.append(j_beta)
+	print("Train accuracy %f" % sess.run(accuracy, feed_dict={x: x_train, y: y_train}))
+	exh, exy, j_loss = sess.run([h_sig, y, loss], feed_dict={x: x_train, y: y_train, beta: 0})
+	j_tr_loss.append(j_loss)
+	# print(exh)
+	print("Train AUC %f" % roc_auc_score(exy[:,0],exh[:,0]))
+	print("Validation accuracy %f" % sess.run(accuracy, feed_dict={x: x_validation, y: y_validation}))
+	exh, exy, j_loss = sess.run([h_sig, y, loss], feed_dict={x: x_validation, y: y_validation, beta: 0})
+	j_vl_loss.append(j_loss)
+	print("Validation AUC %f" % roc_auc_score(exy[:,0],exh[:,0]))	
+	j_beta *= mult
 
-plt.plot(tr_loss_lst, 'b')
-plt.plot(ts_loss_lst, 'g')
-plt.show()
+# Plot loss and validation accuracy
+# plt.plot(tr_loss_lst, 'b')
+# plt.plot(ts_loss_lst, 'g')
+
+# Plot train and validation loss vs regularization parameter
+# plt.plot(j_beta_lst, j_tr_loss, 'b')
+# plt.plot(j_beta_lst, j_vl_loss, 'g')
+
+# plt.show()
 
 # Copy the results to a pandas dataframe with an "id" column and
 # a "sentiment" column
-# output = pd.DataFrame( data={"id":test["id"], "sentiment":result} )
+test_data = pd.read_csv("Clean_test.csv", names=['id', 'reviews'], quoting=3)
+# print(test_data['id'].shape)
+x_test = (vectorizer.fit_transform(test_data['reviews'])).toarray()
+# print(x_test)
+result, _ = sess.run([h_sig, h], feed_dict={x: x_test})
+print(result)
+output = pd.DataFrame( data={"id":test_data["id"], "sentiment":result[:,0]} )
 
 # Use pandas to write the comma-separated output file
-# output.to_csv( "Bag_of_Words_model.csv", index=False, quoting=3 )
+output.to_csv( "result.csv", index=False, quoting=3 )
